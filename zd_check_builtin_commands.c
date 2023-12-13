80af5ac0b98e22460f0b7dfbb4717a3374aff61c
zizotopia <zizoknightboy07@yahoo.com>
2023-12-13 15:14:08 -0800

    Hi

---
+++ zd_check_builtin_commands.c
@@ -0,0 +1,49 @@
+#include "main.h"
+
+/**
+ * zd_builtin_command - Function excute builtin commands
+ * @programe_name: command name
+ * @counter: number of commands
+ * @stat: status of process
+ * @lineptr: command builtin
+ * @lin: line
+ * Return: return (-1) or (i)
+*/
+
+int zd_builtin_command(char *programe_name, char *lineptr,
+int *stat, int counter, char *lin)
+{
+int num = 1;
+int i;
+char *cmd_copy;
+char *built_command[] = {"exit", "env", "echo"};
+
+cmd_copy = strdup(lineptr);
+cmd_copy = zd_strtok(cmd_copy, " ");
+for (i = 0; i < 2; i++)
+{
+if (zd_strcmp(cmd_copy, built_command[i]) == 0)
+{
+num = 0;
+free(cmd_copy);
+break;
+}
+}
+switch (i)
+{
+case 0:
+zd_exit(lineptr, stat, counter, programe_name, lin);
+break;
+case 1:
+zd_env();
+break;
+}
+if (num)
+{
+free(cmd_copy);
+return (-1);
+}
+else
+return (i);
+}
+doha
---
+++ zd_mynon_interactive.c
@@ -0,0 +1,51 @@
+#include "main.h"
+
+/**
+ * zd_non_interactive_mode - Function that run shell program
+ * @argv: string pointer to command
+ * Return: returns void
+*/
+
+void zd_non_interactive_mode(char **argv)
+{
+char *lineptr = NULL, *ptr, *command_copy,
+**command_argv = {NULL}, *environ[] = {NULL};
+
+size_t z = 0;
+ssize_t rd;
+int stat = 0, c = 0, i;
+
+while (1)
+{
+c++;
+rd = zd_getline(&lineptr, &z, stdin);
+if (rd == -1)
+{
+break;
+}
+command_copy = zdhandle_new_line(lineptr);
+ptr = get_total_path(command_copy);
+if (zd_strcmp(command_copy, "\n") == 0 || command_copy[0] == '#')
+continue;
+else if (zd_builtin_command(argv[0], command_copy, &stat, c, lineptr) != -1)
+{
+free(ptr);
+continue;
+}
+else if (ptr != NULL)
+{
+command_argv = zd_argv(command_copy);
+i = zd_excut(ptr, command_argv, environ, &stat);
+if (i == 0)
+zd_print_errors(argv[0], c, stat, command_copy);
+}
+else if (ptr == NULL)
+{
+zdexcute_notFound(command_copy, command_argv, environ, &stat);
+zd_print_errors(argv[0], c, stat, command_copy);
+}
+free(ptr);
+}
+free(lineptr);
+}
+doha
