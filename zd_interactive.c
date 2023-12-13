0b0383da7127807ccde147b6e2c0ed5e75e9db4b
zizotopia <zizoknightboy07@yahoo.com>
2023-12-13 15:02:58 -0800

    Hi

--- zd_interactive.c
+++ zd_interactive.c
@@ -1,33 +1,31 @@
 #include "main.h"
-
 /**
- * zd_non_interactive_mode - Function that run shell program
- * @argv: string pointer to command
- * Return: returns void
+ * zd_interactive_mode - Function used to run shell in interactive mode
+ * @argv: array pointer to commands
+ * Return: function return void.
 */
-
-void zd_non_interactive_mode(char **argv)
+void zd_interactive_mode(char **argv)
 {
 char *lineptr = NULL, *ptr, *command_copy,
 **command_argv = {NULL}, *environ[] = {NULL};
-
-size_t z = 0;
 ssize_t rd;
-int stat = 0, c = 0, i;
-
+int stat = 0, z = 0, i;
+size_t s = 0;
 while (1)
 {
-c++;
-rd = zd_getline(&lineptr, &z, stdin);
+z++;
+write(1, "$ ", sizeof("$ ") - 1);
+rd = zd_getline(&lineptr, &s, stdin);
 if (rd == -1)
 {
+write(1, "\n", 1);
 break;
 }
 command_copy = zdhandle_new_line(lineptr);
 ptr = get_total_path(command_copy);
 if (zd_strcmp(command_copy, "\n") == 0 || command_copy[0] == '#')
 continue;
-else if (zd_builtin_command(argv[0], command_copy, &stat, c, lineptr) != -1)
+else if (zd_builtin_command(argv[0], command_copy, &stat, z, lineptr) != -1)
 {
 free(ptr);
 continue;
@@ -36,13 +34,14 @@ else if (ptr != NULL)
 {
 command_argv = zd_argv(command_copy);
 i = zd_excut(ptr, command_argv, environ, &stat);
+
 if (i == 0)
-zd_print_errors(argv[0], c, stat, command_copy);
+zd_print_errors(argv[0], z, stat, command_copy);
 }
 else if (ptr == NULL)
 {
 zdexcute_notFound(command_copy, command_argv, environ, &stat);
-zd_print_errors(argv[0], c, stat, command_copy);
+zd_print_errors(argv[0], z, stat, command_copy);
 }
 free(ptr);
 }
