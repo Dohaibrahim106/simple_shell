1af34860515a7a931b7d705e12b9cb1114656d57
zizotopia <zizoknightboy07@yahoo.com>
2023-12-13 14:59:50 -0800

    Hi

--- zd_interactive.c
+++ zd_interactive.c
@@ -1,12 +1,12 @@
 #include "main.h"
 
 /**
- * my_non_interactive_mode - Function that run shell program
+ * zd_non_interactive_mode - Function that run shell program
  * @argv: string pointer to command
  * Return: returns void
 */
 
-void my_non_interactive_mode(char **argv)
+void zd_non_interactive_mode(char **argv)
 {
 char *lineptr = NULL, *ptr, *command_copy,
 **command_argv = {NULL}, *environ[] = {NULL};
@@ -18,31 +18,31 @@ int stat = 0, c = 0, i;
 while (1)
 {
 c++;
-rd = my_getline(&lineptr, &z, stdin);
+rd = zd_getline(&lineptr, &z, stdin);
 if (rd == -1)
 {
 break;
 }
-command_copy = myhandle_new_line(lineptr);
+command_copy = zdhandle_new_line(lineptr);
 ptr = get_total_path(command_copy);
-if (my_strcmp(command_copy, "\n") == 0 || command_copy[0] == '#')
+if (zd_strcmp(command_copy, "\n") == 0 || command_copy[0] == '#')
 continue;
-else if (my_builtin_command(argv[0], command_copy, &stat, c, lineptr) != -1)
+else if (zd_builtin_command(argv[0], command_copy, &stat, c, lineptr) != -1)
 {
 free(ptr);
 continue;
 }
 else if (ptr != NULL)
 {
-command_argv = my_argv(command_copy);
-i = my_excut(ptr, command_argv, environ, &stat);
+command_argv = zd_argv(command_copy);
+i = zd_excut(ptr, command_argv, environ, &stat);
 if (i == 0)
-my_print_errors(argv[0], c, stat, command_copy);
+zd_print_errors(argv[0], c, stat, command_copy);
 }
 else if (ptr == NULL)
 {
-myexcute_notFound(command_copy, command_argv, environ, &stat);
-my_print_errors(argv[0], c, stat, command_copy);
+zdexcute_notFound(command_copy, command_argv, environ, &stat);
+zd_print_errors(argv[0], c, stat, command_copy);
 }
 free(ptr);
 }
