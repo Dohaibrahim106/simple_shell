39f8338f4679c3c5372587c07f84d56bcc51f98d
zizotopia <zizoknightboy07@yahoo.com>
2023-12-13 09:42:46 -0800

    Hi

--- handle_command_line.c
+++ handle_command_line.c
@@ -8,27 +8,27 @@
 
 char *zdhandle_new_line(char *line)
 {
-int len, i = 0;
+	int len, i = 0;
 
-while (line[i])
-{
+	while (line[i])
+	{
 
-if (line[i] != ' ')
-{
-line += i;
-break;
-}
-i++;
-}
-len = (int)zd_strlen(line);
+		if (line[i] != ' ')
+		{
+			line += i;
+			break;
+		}
+		i++;
+	}
+	len = (int)zd_strlen(line);
 
-if (len == 1 && line[0] == '\n')
-{
-return (line);
-}
-len = (int)zd_strlen(line);
+	if (len == 1 && line[0] == '\n')
+	{
+		return (line);
+	}
+	len = (int)zd_strlen(line);
 
-if (line[len - 1] == '\n')
-line[len - 1] = '\0';
-return (line);
+	if (line[len - 1] == '\n')
+		line[len - 1] = '\0';
+	return (line);
 }
